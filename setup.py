73f1dfd929815fdc507d73f58076470c54317c34
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-20 14:54:24 -0600

    CORE-472 prep for internal release

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version('0.1', __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
 
 setup(
     name = 'oauth-client',
-    version = '0.1',
+    version = version,
     packages = ['oauth_client', 'oauth_client.adapter'],
-    
+
     author = 'Lann Martin',
     author_email = 'oauth-client@lannbox.com',
     description = 'client library for OAuth 1.0',
